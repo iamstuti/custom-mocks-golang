13ce92843b716fb6c9a50e3bb1d3ebab2276ba3a
iamstuti <stuti.2996@gmail.com>
2020-09-14 01:06:55 +0530

    added setup.sh file that lists dependencies

---
+++ src/setup.sh
@@ -0,0 +1,16 @@
+#Mux
+go get -u -v github.com/gorilla/mux
+#gometalinter --install
+go get github.com/kujtimiihoxha/kit
+#Go-kit
+go get -u -v github.com/go-kit/kit
+go get github.com/go-kit/kit/endpoint
+go get -v -u github.com/go-kit/kit/transport/http
+go get github.com/go-logfmt/logfmt
+go get -u -v cloud.google.com/go/datastore
+go get -u -v google.golang.org/api/googleapi
+go get github.com/googleapis/google-cloud-go-testing
+go get -u gopkg.in/resty.v1
+#For SendGrid
+go get -v github.com/sendgrid/sendgrid-go
+
